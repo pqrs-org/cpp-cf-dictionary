6141a383daf6fddfb2200e89e19841d5f986d71b
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-22 19:41:35 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,18 +1,23 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/cf/dictionary.hpp>
 
-TEST_CASE("make_cf_mutable_dictionary") {
-  {
-    auto dictionary = pqrs::cf::make_cf_mutable_dictionary();
-    REQUIRE(dictionary);
-    REQUIRE(CFGetRetainCount(*dictionary) == 1);
-    REQUIRE(CFDictionaryGetCount(*dictionary) == 0);
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-    CFDictionarySetValue(*dictionary, CFSTR("key1"), CFSTR("value1"));
-    CFDictionarySetValue(*dictionary, CFSTR("key2"), CFSTR("value2"));
+  "make_cf_mutable_dictionary"_test = [] {
+    {
+      auto dictionary = pqrs::cf::make_cf_mutable_dictionary();
+      expect(dictionary);
+      expect(CFGetRetainCount(*dictionary) == 1);
+      expect(CFDictionaryGetCount(*dictionary) == 0);
 
-    REQUIRE(CFDictionaryGetCount(*dictionary) == 2);
-  }
+      CFDictionarySetValue(*dictionary, CFSTR("key1"), CFSTR("value1"));
+      CFDictionarySetValue(*dictionary, CFSTR("key2"), CFSTR("value2"));
+
+      expect(CFDictionaryGetCount(*dictionary) == 2);
+    }
+  };
+
+  return 0;
 }
