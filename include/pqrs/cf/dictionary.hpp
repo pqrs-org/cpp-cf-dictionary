a6d6d617cdc4e0bf6cfc6bb3cf6b2443f2bac1cb
Takayama Fumihiko <tekezo@pqrs.org>
2022-06-05 21:32:47 +0900

    Add make_cf_mutable_dictionary_copy

--- include/pqrs/cf/dictionary.hpp
+++ include/pqrs/cf/dictionary.hpp
@@ -10,13 +10,27 @@
 
 namespace pqrs {
 namespace cf {
-static cf_ptr<CFMutableDictionaryRef> make_cf_mutable_dictionary(CFIndex capacity = 0) {
+inline cf_ptr<CFMutableDictionaryRef> make_cf_mutable_dictionary(CFIndex capacity = 0) {
   cf_ptr<CFMutableDictionaryRef> result;
 
   if (auto cf_mutable_dictionary = CFDictionaryCreateMutable(kCFAllocatorDefault,
                                                              capacity,
                                                              &kCFTypeDictionaryKeyCallBacks,
                                                              &kCFTypeDictionaryValueCallBacks)) {
+    result = cf_mutable_dictionary;
+    CFRelease(cf_mutable_dictionary);
+  }
+
+  return result;
+}
+
+inline cf_ptr<CFMutableDictionaryRef> make_cf_mutable_dictionary_copy(CFDictionaryRef dictionary,
+                                                                      CFIndex capacity = 0) {
+  cf_ptr<CFMutableDictionaryRef> result;
+
+  if (auto cf_mutable_dictionary = CFDictionaryCreateMutableCopy(kCFAllocatorDefault,
+                                                                 capacity,
+                                                                 dictionary)) {
     result = cf_mutable_dictionary;
     CFRelease(cf_mutable_dictionary);
   }
